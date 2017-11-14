412bbe19f1158418702219586aa75669b5cb1ff4
ashleyrwharton <awharton@andigital.com>
2017-11-14 13:57:24 +0000

    Add ESlint and Prettier (#5)

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,24 +1,32 @@
-const MongoClient = require('mongodb').MongoClient
-const config = require('../config')
+const MongoClient = require("mongodb").MongoClient;
+const config = require("../config");
 
-const users = {}
+const users = {};
 
-users.list = (req, res) => MongoClient.connect(config.mongoURL, (err, db) => {
-  if (err) throw err
-  db.collection(config.mongoCollection).find().toArray((err, result) => {
-    if (err) throw err
-    res.json(result)
-    db.close()
-  })
-})
+users.list = (req, res) =>
+  MongoClient.connect(config.mongoURL, (err, db) => {
+    if (err) throw err;
+    db
+      .collection(config.mongoCollection)
+      .find()
+      .toArray((err, result) => {
+        if (err) throw err;
+        res.json(result);
+        db.close();
+      });
+  });
 
-users.read = (req, res) => MongoClient.connect(config.mongoURL, (err, db) => {
-  if (err) throw err
-  db.collection(config.mongoCollection).find({ _id: req.params.id }).toArray((err, result) => {
-    if (err) throw err
-    res.json(result)
-    db.close()
-  })
-})
+users.read = (req, res) =>
+  MongoClient.connect(config.mongoURL, (err, db) => {
+    if (err) throw err;
+    db
+      .collection(config.mongoCollection)
+      .find({ _id: req.params.id })
+      .toArray((err, result) => {
+        if (err) throw err;
+        res.json(result);
+        db.close();
+      });
+  });
 
-module.exports = users
+module.exports = users;
