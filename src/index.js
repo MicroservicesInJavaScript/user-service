35f5f056bd1f4d5fdc5fd39acae9f6510f7f0c57
Oliver Smit <olmesm@gmail.com>
2017-11-14 12:36:27 +0000

    added static route for docs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+const path = require('path');
 const express = require('express')
+
 const app = express()
 const config = require('./config')
 
 // Load Controllers
 const users = require('./controllers/users.js')
+
+// Api Docs endpoint
+app.use('/docs', express.static(path.join(__dirname, 'api-docs')))
 
 // Health check endpoint
 app.get('/status', (req, res) => res.status(200).send('OK'))
