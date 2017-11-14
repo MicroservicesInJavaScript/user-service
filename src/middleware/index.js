88ce2ae32ba2bdea3273af73d4de2b5b7a97e706
Ashley <wharton.ash@gmail.com>
2017-11-14 19:50:00 +0000

    Debugging failing build

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,9 +3,9 @@ const morgan = require("morgan");
 const logstream = require("../services/logstream");
 
 const morganLogstream = logstream({
-  logPath: process.env.REQUST_LOGGER_PATH,
-  interval: process.env.REQUST_LOGGER_INTERVAL,
-  isSaveEnabled: process.env.REQUST_LOGGER_SAVE_LOGS
+  logPath: process.env.REQUEST_LOGGER_PATH,
+  interval: process.env.REQUEST_LOGGER_INTERVAL,
+  isSaveEnabled: process.env.REQUEST_LOGGER_SAVE_LOGS
 });
 
 module.exports = [morgan("combined", { stream: morganLogstream })];
