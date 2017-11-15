1c3c4c9f42fcef94306921ef3b293fe333e12ded
Oliver Smit <olmesm@gmail.com>
2017-11-15 14:40:06 +0000

    fixed test

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -1,6 +1,9 @@
 const mockedDB = require("./__mocks__/db_mock");
-
 const users = require("../controllers/users.js");
+
+jest.mock("../models/db", () => {
+  return { dbConnection: () => mockedDB };
+});
 
 describe("gets users", () => {
   let req = {};
@@ -12,7 +15,7 @@ describe("gets users", () => {
 
   beforeEach(() => {});
   it("should load users list", done => {
-    let usersData = users.list(req, res, mockedDB);
+    let usersData = users.list(req, res);
 
     expect(mockedDB.collection.mock.calls.length).toBe(1);
     done();
