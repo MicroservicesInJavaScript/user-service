90833a882bc14fdbdedefc0dc5dc59daf98673ff
Ellie <eleanorhamilton77@gmail.com>
2017-11-15 14:07:10 +0000

    Feature/fix user endpoints (#17)
    * fix user endpoints
    
    * fix authorities endpoint
    
    * linting

--- src/index.js
+++ src/index.js
@@ -15,6 +15,15 @@ const { MONGO_URL, PORT } = process.env;
 const app = express();
 app.use(middleware);
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true
+  })
+);
+
 // Api Docs endpoint
 app.use("/docs", express.static(path.join(__dirname, "api-docs")));
 
@@ -25,16 +34,8 @@ MongoClient.connect(MONGO_URL, (err, db) => {
   if (err) error(err);
 
   // User APIs
-  app.get("/", (req, res) => {
-    return users.list(req, res, db);
-  });
-
-  app.post("/", (req, res) => {
-    return users.add(req, res, db);
-  });
-
-  app.put("/", (req, res) => {
-    return users.update(req, res, db);
+  app.get("/authorities", (req, res) => {
+    return authorities.read(req, res, db);
   });
 
   app.delete("/:login", (req, res) => {
@@ -45,8 +46,16 @@ MongoClient.connect(MONGO_URL, (err, db) => {
     return users.read(req, res, db);
   });
 
-  app.get("/authorities", (req, res) => {
-    return authorities.read(req, res, db);
+  app.get("/", (req, res) => {
+    return users.list(req, res, db);
+  });
+
+  app.post("/", (req, res) => {
+    return users.add(req, res, db);
+  });
+
+  app.put("/", (req, res) => {
+    return users.update(req, res, db);
   });
 
   // Listen on app port
